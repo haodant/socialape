3ec3b982f2a7b9c98320feba3d22b31d741f5a15
Haodan Tan <haodantan@Haodans-MacBook-Air-2.local>
2019-08-26 21:11:29 -0400

    first commit

---
+++ socialape-functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is the scream body',
+      createdAt: "2019-08-26T21:23:36.573Z",
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+}
